ca26dc502c639bfc087e3343d965f5a91a44819d
pauloacf <pauloacf148@gmail.com>
2021-05-29 01:40:47 -0400

    Test

--- plebot.py
+++ plebot.py
@@ -1,9 +1,11 @@
 import re
 import os
-from flask import Flask
+from flask import Flask, jsonify
+import web
 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def Main():
@@ -14,10 +16,9 @@ def Main():
     def verifyMessage(message, question, response):
         verify = re.search(question, message, re.IGNORECASE)
         if (verify):
-            print(response)
-            return(True)
+            return "<h1>"+response+"</h1>"
 
-    # controla o fluxo de mensagens do plebot        
+    # controla o fluxo de mensagens do plebot
     def plebotMessage(message):
         check = 'false'
         for i in range(len(listResponses)):
@@ -25,40 +26,39 @@ def Main():
                 check = 'true'
                 plebotExit(len(listResponses), i)
                 break
-        
-        if (check == 'false'):
-            print("Não entendo tais palavras, é melhor que você não esteja fazendo "
-                + "bruxaria.")
 
-    # controla quando a conversação deve terminar            
+        if (check == 'false'):
+            return "<h1>Não entendo tais palavras, é melhor que você não esteja fazendo bruxaria.</h1>"
+
+    # controla quando a conversação deve terminar
     def plebotExit(listLen, listPos):
-        if( listPos == listLen - 1 or listPos == listLen - 2 or 
-            listPos == listLen - 3 or listPos == listLen - 4 ):
+        if(listPos == listLen - 1 or listPos == listLen - 2 or
+                listPos == listLen - 3 or listPos == listLen - 4):
             exit(1)
 
     '''
     RESPOSTAS DO PLEBOT
     '''
-                    # Saudações
-    listResponses = [[r'boa tarde', "Saudações nobre cliente"], 
-                    [r'boa noite', "Saudações nobre cliente"],
-                    [r'bom dia', "Saudações nobre cliente"],
-                    [r'eae', "Saudações nobre cliente"], 
-                    [r'olá', "Saudações nobre cliente"], 
-                    [r'ola', "Saudações nobre cliente"],
-                    [r'oi', "Saudações nobre cliente"],
-                    # Despedidas
-                    [r'até mais', "Volte sempre que quiser o melhor produto"],
-                    [r'até a próxima', "Até a próxima, forasteiro..."],
-                    [r'adeus', "Que os deuses lhe guiem!"],
-                    [r'tchau', "Vá pela sombra jovem!"]]
+    # Saudações
+    listResponses = [[r'boa tarde', "Saudações nobre cliente"],
+                     [r'boa noite', "Saudações nobre cliente"],
+                     [r'bom dia', "Saudações nobre cliente"],
+                     [r'eae', "Saudações nobre cliente"],
+                     [r'olá', "Saudações nobre cliente"],
+                     [r'ola', "Saudações nobre cliente"],
+                     [r'oi', "Saudações nobre cliente"],
+                     # Despedidas
+                     [r'até mais', "Volte sempre que quiser o melhor produto"],
+                     [r'até a próxima', "Até a próxima, forasteiro..."],
+                     [r'adeus', "Que os deuses lhe guiem!"],
+                     [r'tchau', "Vá pela sombra jovem!"]]
 
     '''
     FLUXO DO PLEBOT
     '''
 
     while(True):
-        message = input()
+        message = web.input()
         plebotMessage(message)
 
 
