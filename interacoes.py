6db01cb0fecb19a354021c0b72cb35b3aef289e5
murilloacacio <mdla.snf19@uea.edu.br>
2021-05-30 19:24:24 -0400

    update de interaçoes

--- interacoes.py
+++ interacoes.py
@@ -2,15 +2,25 @@
 RESPOSTAS DO PLEBOT
 '''
                 # Saudações
-listResponses = [[r'boa tarde', "Saudações nobre cliente"], 
-                [r'boa noite', "Saudações nobre cliente"],
-                [r'bom dia', "Saudações nobre cliente"],
-                [r'eae', "Saudações nobre cliente"], 
-                [r'olá', "Saudações nobre cliente"], 
-                [r'ola', "Saudações nobre cliente"],
-                [r'oi', "Saudações nobre cliente"],
+listResponses = [[r'tarde|noite|dia|eae|olá|ola|oi', "\nSaudações nobre cliente"],
+
+                [r'comida|cardapio',
+                "\nAqui está o nosso cardapio:"
+                "\n- Hambúrguer Rei Arthur      . R$ 25.00"
+                "\n- Porção de batatas mágicas  . R$ 10.00"
+                "\n- Esfirra secreta de frango  . R$ 7.50"],
+
+                [r'hamburguer|hambúrguer|batata|esfirra', "\nBoa escolha forasteiro"],
+
+                [r'bebida',
+                "\nAinda não temos nada, acabou o rum e só tem água"],
+
+                [r'bem|como vai', "\nVou ficar melhor se comprar alguma coisa"],
+
+                [r'caro', "\ntenho que pagar minhas contas"],
+
                 # Despedidas
-                [r'até mais', "Volte sempre que quiser o melhor produto"],
-                [r'até a próxima', "Até a próxima, forasteiro..."],
-                [r'adeus', "Que os deuses lhe guiem!"],
-                [r'tchau', "Vá pela sombra jovem!"]]
+                [r'até mais', "\nVolte sempre que quiser o melhor produto"],
+                [r'até a próxima', "\nAté a próxima, forasteiro..."],
+                [r'adeus', "\nQue os deuses lhe guiem!"],
+                [r'tchau', "\nVá pela sombra jovem!"]]
