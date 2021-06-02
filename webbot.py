145b7915ac44abaef56973215763c7b7c071d231
natanaelmota1 <ndmf.snf19@uea.edu.br>
2021-06-02 14:46:15 -0400

    julgamento??

--- webbot.py
+++ webbot.py
@@ -13,7 +13,7 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/')
+@app.route('/plebot-chatbot')
 def home():
     return render_template('chat.html')
 
