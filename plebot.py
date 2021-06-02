b6e6d864f9e7db813276036bcfdbfa6376971bae
natanaelmota1 <ndmf.snf19@uea.edu.br>
2021-06-01 20:31:35 -0400

    funcionando ALELUIA : )

--- plebot.py
+++ plebot.py
@@ -1,47 +1,23 @@
 import re
 import unicodedata
-from webbot import *
 from interacoes import *
 
 '''
-MÉTODOS DO PLEBOT                                  
+MÉTODO DO PLEBOT                                  
 '''
-# verifica qual é a mensagem do usuário e se há resposta do plebot
-def verifyMessage(message, question, response):
-    verify = re.search(question, message, re.IGNORECASE)
-    if (verify):
-        print("PLEBOT:"+ response)
-        return(True)
+# controla o fluxo de mensagens do plebot
 
-# controla o fluxo de mensagens do plebot        
 def plebotMessage(message):
 
     # removendo caracteres especiais
     process = unicodedata.normalize("NFD", message)
     process = process.encode("ascii", "ignore")
     process = process.decode("utf-8")
-    
-    check = 'false'
+
     for i in range(len(listResponses)):
-        if(verifyMessage(process, listResponses[i][0], listResponses[i][1])):
-            check = 'true'
-            plebotExit(len(listResponses), i)
-            break
-    
-    if (check == 'false'):
-        print("Não entendo tais palavras, é melhor que você não esteja fazendo "
-            + "bruxaria.")
+        verify = re.search(listResponses[i][0], process, re.IGNORECASE)
+        if (verify):
+            return({'message': "PLEBOT:" + listResponses[i][1]})
 
-# controla quando a conversação deve terminar            
-def plebotExit(listLen, listPos):
-    if( listPos == listLen - 1 or listPos == listLen - 2 or 
-        listPos == listLen - 3 or listPos == listLen - 4 ):
-        exit(1)
+    return({'message': "Não entendo tais palavras, é melhor que você não esteja fazendo bruxaria."})
 
-'''
-FLUXO DO PLEBOT
-'''
-
-while(True):
-    message = input("\n>>>     ")
-    plebotMessage(message)
--- webbot.py
+++ webbot.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask.templating import render_template
 from flask_socketio import SocketIO, emit
+from plebot import plebotMessage
+
 
 app = Flask(__name__)
 io = SocketIO(app)
@@ -13,6 +15,7 @@ def home():
 def send_message_handler(msg):
     print(input)
     emit('getMessage', msg, json=True)
+    emit('getMessage', plebotMessage(str(msg)), json=True)
 
 if __name__ == '__main__':
     io.run(app, debug=True)
