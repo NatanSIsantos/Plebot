2026d66723186d646491cdbf679d6a3aa9900d6d
natanaelmota1 <ndmf.snf19@uea.edu.br>
2021-06-02 15:02:52 -0400

    vamo que vvamo

--- webbot.py
+++ webbot.py
@@ -8,6 +8,10 @@ from flask import send_from_directory
 app = Flask(__name__)
 io = SocketIO(app)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def home():
